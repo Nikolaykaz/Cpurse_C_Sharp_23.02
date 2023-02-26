191a1ffdd0b6756c71fcb1c5d27c3d626934e3c6
Nikolaykaz <nikolaykaz@gmail.com>
2023-02-26 16:17:12 +0300

    Task 1_0 ver 01

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+string s = Console.ReadLine()!;
+int num = int.Parse(s);
+Console.WriteLine(num * num);
