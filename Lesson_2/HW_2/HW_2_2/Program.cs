7dde7979184649456a1ffb1c0a52294e7bbd2c59
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-05 18:08:44 +0300

    HomeWork 2_2

---
+++ Lesson_2/HW_2/HW_2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 100)
+{
+    Console.WriteLine("Введенное число не трехзначное! ");
+}
+else
+{
+    int result = Number(a); 
+    Console.Write($"Третья цифра числа: {Number(a)}");
+}
+
+int Number(int a)
+{
+    while (a > 999)
+    {
+        a /= 10;
+    }       
+    return a % 10;
+}
