62af687ab3d824cf63f3a6655d23794c53ba8b45
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-07 17:19:08 +0300

    Lesson_3_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите крограмму которая, принимает на вход число (N)
+// и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+int num = int.Parse(Console.ReadLine()!);
+
+void Div(int n)
+{
+    int i = 1;
+    while(n >= i)
+    {
+        Console.WriteLine(Math.Pow(i, 2));
+        i++;
+    }
+}
+Div(num);
