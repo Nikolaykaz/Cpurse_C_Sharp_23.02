3947dd66cc918af07562cac70d6a866a05c47100
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-07 15:58:07 +0300

    Lesson_3_1

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон
+// возможных координат точек в этой четверти (х и у).
+
+void Coordinates(int num)
+
+{
+    if (num == 1) 
+        Console.WriteLine("х > 0 и y > 0 ");
+    else if (num == 2)
+        Console.WriteLine("х < 0 и y > 0 ");
+    else if (num == 3)
+        Console.WriteLine("х < 0 и y < 0 ");
+    else if (num == 4)
+        Console.WriteLine("х > 0 и y < 0 ");
+    else
+        Console.WriteLine("Неверное значение четверти!");
+}
+
+Console.Write("Введите четверть=");
+int num = int.Parse(Console.ReadLine()!);
+Coordinates(num);
