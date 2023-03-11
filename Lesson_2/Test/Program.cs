ccc50d432a478143aa439c0d1cf9fb5289f6938d
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-11 18:15:40 +0300

    Lecture

--- Lesson_2/Test/Program.cs
+++ Lesson_2/Test/Program.cs
@@ -5,6 +5,6 @@ int num1 = num;
 Console.WriteLine(num / 1000 % 10);
 Console.Write(num % 100 / 10);
 
-// if (num / 10000 == num % 10 && num1 / 1000 % 10 == num1 % 100 / 10)
+// 
 //        Console.Write("Это число палиндром!");
 //    else Console.Write("Это число НЕ палиндром!");
---
+++ Lesson_3/Lecture/Program.cs
@@ -0,0 +1,76 @@
+﻿// Метод 1 - не принимают и не возвращают
+//void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+//Method1();
+
+// Метод 2 - ничего не возвращает, но принимает
+//void Method2(string msg)
+//{
+//    Console.WriteLine(msg);
+//}
+//Method2("Текст сообщения");
+
+// Метод 2_1 - Именованные аргументы. Какому аргументу,
+// какое значение мы хотим указать. Прервать зацыкленную программу можно Ctrl + "C"
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//     Console.WriteLine(msg);
+//     i++;
+//     }
+// }
+// Method21(msg: "Текст сообщения", count: 4); // явно передали значение текст msg: "Текст" 
+
+// Метод 3. Ничего не принимает, что то возвращает.
+// Указываем тип данных, который мы ожидаем. return...
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// Метод 4. Что то принимают и что то возвращают.
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty; // Значение resalt будет пустым string.Empty
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "qwer");
+// Console.WriteLine(res);
+
+string Method4(int count, string text)
+{
+    string result = string.Empty; // Значение resalt будет пустым string.Empty
+    for(int i = 0;i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "qwer");
+Console.WriteLine(res);
+
+// Цикл в цикле For
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
