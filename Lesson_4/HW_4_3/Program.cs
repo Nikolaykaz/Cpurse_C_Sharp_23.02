46abaa7b3268fe7a6c7fd1f606df02962ff056b5
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-13 00:01:26 +0300

    HW_4_3

--- Lesson_4/HW_4_3/Program.cs
+++ Lesson_4/HW_4_3/Program.cs
@@ -3,27 +3,34 @@
 // и вывода в виде функции.
 // Две функции 1. Заполнение массива. 2. Вывод массива.
 
-int Massive(int[] array)
-{
-    // Заполнение массива согласно введенным данных пользователя.
+// Сняли показания пользователя о массиве.
     Console.Write("Сколько элементов будет в массиве? ");
     int num = int.Parse(Console.ReadLine()!);
-
-    int[] numbers = new int[num]; // Создание целочисленного массива numbers, mas - это из скольки элементов состоит массив.
-    
     Console.Write("Какой будет диапазон чисел, каждого элемента массива? (Пример. От: 5 До: 30) ");
     Console.Write("От: ");
     int min = int.Parse(Console.ReadLine()!);
     Console.Write("До: ");
     int max = int.Parse(Console.ReadLine()!);
-    
-    for (int i = 0; i < num; i++)
-    {
 
-        numbers[i] = new Random().Next(min, max + 1); // Диапазон "Рандома", до какого значения, нужно увеличивать на 1.
+// Передали данные.   
+int[] mass = Massive(num, min, max);
+Output(mass);
+
+// Сборка массива.
+int[] Massive(int num, int min, int max) 
+{  
+    int[] numbers = new int[num];
+    for (int i = 0; i < num; i++)
+    numbers[i] = new Random().Next(min, max); // Диапазон "Рандома", до какого значения, нужно увеличивать на 1.
                                               // В нашем случае это .Next(0, 2). А числа будут "Рандомить" 0 и 1.
                                               // Можно оставить Next(2), потому что все равно стартует с 0.
-        Console.WriteLine(numbers[i]);
-    }
+    return numbers;
 }
-Massive();
+
+// Вывод массива.
+void Output(int[] numbers)
+{
+    int num = numbers.Length; // Количество элементов в массиве.
+    for (int i = 0; i < num; i++)
+        Console.Write($"{numbers[i]},");
+}
