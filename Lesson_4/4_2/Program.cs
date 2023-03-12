1ab16a4c4f3f7eb32e9af54ed5523015780bb5bf
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-12 16:07:47 +0300

    Lesson_4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4.2 Напишите программу, которая принимает на вход число N и
+// выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int CompositionNum(int num)
+{
+    int multiply = 1;
+    for (int i = 1; num >= i; i++)
+        multiply *= i;
+
+    return multiply;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(A);
+Console.WriteLine(result);
