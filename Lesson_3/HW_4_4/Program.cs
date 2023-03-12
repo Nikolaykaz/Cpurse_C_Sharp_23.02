49a11ef2d46aa0b613a3a7891a4df6ede9cb82b6
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-12 15:12:43 +0300

    Lesson_4_0

---
+++ Lesson_3/HW_4_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+// 3 -> 1,8,27
+// 5 -> 1,8,27,64,125
+void Squares(int n)
+{
+    double[] nums = new double[n]; // Создание массива n элементов массива и типом данных double. Он заполняется нулями.
+    int index = 0;
+    Console.Write($"{n} -> ");
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 3); // Запись в ячейку index значения index + 1 в кубе.
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }
+    Console.WriteLine();
+}
+Squares(int.Parse(Console.ReadLine()!));
---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4.0. НАпишите программу, которая принимает на вход число (А) и 
+// выдает сумму чисел от 1 до А. Используем цикл For.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNum(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+        all_sum += i;
+
+    return all_sum;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
