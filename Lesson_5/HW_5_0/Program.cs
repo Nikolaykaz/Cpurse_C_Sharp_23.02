89d9c8858fe06bc35420ce7b5b99e5ca9baac8fe
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-15 19:14:12 +0300

    HW_5_3

--- Lesson_5/HW_5_0/Program.cs
+++ Lesson_5/HW_5_0/Program.cs
@@ -1,1 +1,65 @@
-﻿// 
+﻿// Проблема в неявном преобразовании переменной. double в int
+
+
+// Сняли показания пользователя о массиве.
+Console.Write("Сколько элементов будет в массиве? ");
+double num = int.Parse(Console.ReadLine()!);
+Console.Write("Какой будет диапазон чисел, каждого элемента массива? (Пример. От: 5 До: 30) ");
+Console.Write("От: ");
+double min = int.Parse(Console.ReadLine()!);
+Console.Write("До: ");
+double max = int.Parse(Console.ReadLine()!);
+
+// Описали, какой будет массив, сколько элементов, какой рандом каждого элемента.   
+double[] mass = Massive(num, min, max);
+
+// Выводим наш массив. 
+Output(mass);
+// Находим разницу между максимальным и минимальным элементом массива.
+double diff = FindDifference(mass);
+// Выводим разницу между мин-макс.
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {diff}");
+
+// Заполнение массива по входным данным.
+double[] Massive(double num, double min, double max)
+{
+    double[] numbers = new double[num];
+    for (int i = 0; i < num; i++)
+    {
+        numbers[i] = new Random().NextDouble(min, max);
+        Console.WriteLine(numbers[i]);
+    }
+    return numbers;
+}
+
+// Вывод массива.
+void Output(double[] numbers)
+{
+    double num = numbers.Length; // Количество элементов в массиве.
+    for (int i = 0; i < num; i++)
+        Console.Write($"{numbers[i]} ");
+}
+
+// Вычисление разницы между миним. и макс. значением элементов массива.
+double FindDifference(double[] array)
+{
+    double max = array[0]; // Предполагаем, что первый элемент является максимальным
+    double min = array[0]; // Предполагаем, что первый элемент является минимальным
+
+    // Находим максимальный и минимальный элементы массива
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    // Вычисляем разницу между максимальным и минимальным элементом массива
+    double diff = max - min;
+    // Возвращаем разницу между максимальным и минимальным элементом массива
+    return diff;
+}
---
+++ Lesson_5/HW_5_3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 3. Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементом массива.
+// [3, 7, 22, 2, 78] -> 76
+
+
+double[] array = Massive();
+
+double diff = FindDifference(array);
+
+Output(array);
+
+// Вывод массива.
+void Output(double[] array)
+{
+    double num = array.Length; // Количество элементов в массиве.
+    for (int i = 0; i < num; i++)
+        Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine($"Разница между макс. и мин. элементом массива = {diff}");
+
+double[] Massive()
+{
+    // Создаем массив из 5 вещественных чисел
+    double[] array = new double[5];
+
+    // Заполняем массив случайными числами
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.NextDouble() * 100; // Генерируем случайное вещественное число от 0 до 100
+    }
+
+    // Возвращаем созданный массив
+    return array;
+}
+
+double FindDifference(double[] array)
+{
+    double max = array[0]; // Предполагаем, что первый элемент является максимальным
+    double min = array[0]; // Предполагаем, что первый элемент является минимальным
+
+    // Находим максимальный и минимальный элементы массива
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    // Вычисляем разницу между максимальным и минимальным элементом массива
+    double diff = max - min;
+    // Возвращаем разницу между максимальным и минимальным элементом массива
+    return diff;
+}
