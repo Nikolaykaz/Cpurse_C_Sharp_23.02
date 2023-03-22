ad32b09c521c4e2c6ac9af2073ace4466f65b6b3
Nikolaykaz <nikolaykaz@gmail.com>
2023-03-22 22:54:06 +0300

    HW_7_1

---
+++ Lesson_7/HW_7_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1. Задайте двуменрный массив размером m на n,
+// заполненный случайными вещественными числами.
+// double number = 3.1415926;
+// Console.WriteLine($"Число с округлением до сотых: {number:F2}");
+
+void Print(double[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]:F2} ");
+        Console.WriteLine();
+    }
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(new Random().NextDouble() * (to - from) + from, 2);
+
+    return arr;
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
---
+++ Lesson_7/HW_7_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// 
+// arr[i, j] = Math.Round(new Random().NextDouble() * 2000 - 1000, 2);
+double num = Math.Round(new Random().NextDouble() * 2000 - 1000, 2);
+// arr[i,j] = Math.Round(new Random().NextDouble() * 2000 - 1000, 2);
+Console.WriteLine(num);
+
