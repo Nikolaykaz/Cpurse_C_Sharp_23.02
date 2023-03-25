e780fab23c16012bf7c943da318362053b1ffdaa
Nikolay <nikolaykaz@gmail.com>
2023-03-26 00:58:08 +0300

    Lecture_6

---
+++ Lesson_6/Lecture_6/Program.cs
@@ -0,0 +1,22 @@
+﻿// Что можно улучшить в этом коде?
+
+const double пи = 3.1415;
+int k_BackingField;
+void set_MyProperty (int value)
+{
+  k_BackingField = value;
+}
+int get_MyProperty()
+{
+  return k_BackingField;
+}
+
+
+// int MyProperty { get; set; }
+// const double pi = 3.1415;
+// private int k_BackingField;
+// public int MyProperty
+// {
+//   get { return k_BackingField; }
+//   set { k_BackingField = value; }
+// }
